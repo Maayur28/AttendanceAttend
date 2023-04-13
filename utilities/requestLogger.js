e8a9f0fd41f5c34847e9a24ccf64238fa7be9285
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-04-13 21:31:16 +0530

    added fox

--- utilities/requestLogger.js
+++ utilities/requestLogger.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const requestLogger=(req,res,next)=>{
     let data=`Date- ${new Date()}  Method=${req.method} Url=${req.url}\n`;
-    fs.appendFile("requestLogger.txt",data,(err)=>{
+    fs.appendFile("/tmp/requestLogger.txt",data,(err)=>{
         if(err)
         return next(err);
     })
